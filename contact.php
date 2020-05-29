0aa5353d66f02c8dc0b767215b00d7e5c1e41b31
vakil <48942252+vakilsingh12@users.noreply.github.com>
2020-05-29 11:59:43 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -14,7 +14,7 @@ if(isset($_REQUEST['submit']))
 <!DOCTYPE html>
 <html>
 <head>
-	<title>csenotes12.in</title>
+	<title>phonebook</title>
 <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
